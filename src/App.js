38f54d26e2e695a7d1782ca613b11b459752acbc
Cris10Lobo <cris10lobo@gmail.com>
2022-01-21 19:22:28 +0530

    Added boiler routes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import GoogleSignIn from "./components/GoogleSignIn";
+import CouncSignup from "./pages/CouncSignup";
+import CouncSignin from "./pages/CouncSignin";
+import CouncVerify from "./pages/CouncVerify";
 
 function App() {
-  return (
-    <AuthContextProvider>
-      <div className="App">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="signup" element={<GoogleSignIn />} />
-          </Routes>
-        </Layout>
-      </div>
-    </AuthContextProvider>
-  );
+	return (
+		<AuthContextProvider>
+			<div className="App">
+				<Layout>
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="signup" element={<GoogleSignIn />} />
+						<Route path="councillor/signup" element={<CouncSignup />} />
+						<Route path="councillor/signin" element={<CouncSignin />} />
+						<Route path="councillor/verify" element={<CouncVerify />} />
+					</Routes>
+				</Layout>
+			</div>
+		</AuthContextProvider>
+	);
 }
 
 export default App;
