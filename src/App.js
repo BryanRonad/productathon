cd953631e12139b714b61c94101e1a57ca1e201b
Cris10Lobo <cris10lobo@gmail.com>
2022-01-21 16:51:54 +0530

    added react-icons + basic nav sketch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import AuthContextProvider from "./context/AuthContext";
 import GoogleSignIn from "./components/GoogleSignIn";
+import { Layout } from "./components/Layout";
 
 function App() {
-  return (
-    <AuthContextProvider>
-      <div className="App">
-        <GoogleSignIn />
-      </div>
-    </AuthContextProvider>
-  );
+	return (
+		<AuthContextProvider>
+			<div className="App">
+				<Layout />
+			</div>
+		</AuthContextProvider>
+	);
 }
 
 export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const HomePage = () => {
-  return <div></div>;
+	return (
+		<div>
+			<h1></h1>
+			<br />
+		</div>
+	);
 };
 
 export default HomePage;
