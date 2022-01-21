0776593cd77bada3ffeb7a1581d2280868d0a494
BryanRonad <bryanronad@gmail.com>
2022-01-21 17:38:29 +0530

    React routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
 import AuthContextProvider from "./context/AuthContext";
-import GoogleSignIn from "./components/GoogleSignIn";
+import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import HomePage from "./pages/HomePage";
+import GoogleSignIn from "./components/GoogleSignIn";
 
 function App() {
-	return (
-		<AuthContextProvider>
-			<div className="App">
-				<Layout>
-					<HomePage />
-				</Layout>
-			</div>
-		</AuthContextProvider>
-	);
+  return (
+    <AuthContextProvider>
+      <div className="App">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="signup" element={<GoogleSignIn />} />
+          </Routes>
+        </Layout>
+      </div>
+    </AuthContextProvider>
+  );
 }
 
 export default App;
--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
