e3bb034cbfb5fe8da99af68efacb47940f222fea
Tejasjagdale <69722269+Tejasjagdale@users.noreply.github.com>
2022-01-21 23:22:18 +0530

    chat 80% done

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -5,26 +5,40 @@ import "./Chat.css";
 const Chat = () => {
   const ref = db.collection("chatdb");
   const [chat, setchat] = useState([]);
-  const [data,setData] = useState({
-      sender:'',
-      receiver:'',
-      msg:'',
-      time:''
-  })
+  const [data, setData] = useState({
+    sender: "tejas@gmail.com",
+    receiver: "con1@gmail.com",
+    msg: "",
+    time: "",
+  });
 
   function getchats() {
     ref.onSnapshot((querySnapshot) => {
       const items = [];
       querySnapshot.forEach((doc) => {
-        items.push(doc.data());
+        let temp  = doc.data()
+        temp['id'] = doc.id
+        items.push(temp);
       });
       setchat(items);
     });
   }
 
-  const send=(e)=>{
+  function Deletechat(e){
       e.preventDefault();
+      let id = e.currentTarget.parentElement.id
+      ref.doc(id).delete()
   }
+
+  const send = async (e) => {
+    e.preventDefault();
+
+    var d = new Date(); 
+
+    data.time = d.toLocaleString();
+
+    await ref.add(data)
+  };
 
   useEffect(() => {
     getchats();
@@ -37,21 +51,23 @@ const Chat = () => {
         {chat.map((data) => {
           if (data.sender === "tejas@gmail.com") {
             return (
-              <div className="chat_wrapper">
-                <div id={data.id} key={data.id} className="your_msg">
+              <div className="chat_wrapper" key={data.id}>
+                <div id={data.id} className="your_msg">
                   <h2>you</h2>
                   <p>{data.msg}</p>
                   <p>{data.time}</p>
+                  <button onClick={Deletechat}>X</button>
                 </div>
               </div>
             );
           } else {
             return (
-              <div className="chat_wrapper">
-                <div id={data.id} key={data.id} className="rec_msg">
+              <div className="chat_wrapper" key={data.id}>
+                <div id={data.id} className="rec_msg">
                   <h2>{data.sender}</h2>
                   <p>{data.msg}</p>
                   <b>{data.time}</b>
+                  <button onClick={Deletechat}>X</button>
                 </div>
               </div>
             );
@@ -59,8 +75,17 @@ const Chat = () => {
         })}
       </div>
       <div className="type_msg">
-        <input type="text" name="msg" value={data.msg} />
-        <button className="send_btn">send</button>
+        <input
+          type="text"
+          name="msg"
+          value={data.msg}
+          onChange={(e) => {
+            setData({ ...data, msg: e.currentTarget.value });
+          }}
+        />
+        <button className="send_btn" onClick={send}>
+          send
+        </button>
       </div>
     </>
   );
