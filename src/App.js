4dd015683d022fcab7c36b2163822a0f70b31e43
Cris10Lobo <cris10lobo@gmail.com>
2022-01-21 20:21:07 +0530

    added routes for counc dash

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import GoogleSignIn from "./components/GoogleSignIn";
 import CouncSignup from "./pages/CouncSignup";
 import CouncSignin from "./pages/CouncSignin";
 import CouncVerify from "./pages/CouncVerify";
+import UserDashboard from "./pages/UserDashboard";
+import CouncDashboard from "./pages/CouncDashboard";
 
 function App() {
 	return (
@@ -19,6 +21,8 @@ function App() {
 						<Route path="counsellor/signup" element={<CouncSignup />} />
 						<Route path="counsellor/signin" element={<CouncSignin />} />
 						<Route path="counsellor/verify" element={<CouncVerify />} />
+						<Route path="user/dash" element={<UserDashboard />} />
+						<Route path="counsellor/dash" element={<CouncDashboard />} />
 					</Routes>
 				</Layout>
 			</div>
---
+++ src/pages/CouncDashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CouncDashboard = () => {
+	return (
+		<div>
+			<h1>THis is counsellor dash</h1>
+		</div>
+	);
+};
+
+export default CouncDashboard;
