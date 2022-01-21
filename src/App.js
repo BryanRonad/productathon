7e5be31a1fe05871f86b65482a96fcd96a11a0f8
Cris10Lobo <cris10lobo@gmail.com>
2022-01-21 17:23:36 +0530

    completed homepage sketch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import AuthContextProvider from "./context/AuthContext";
 import GoogleSignIn from "./components/GoogleSignIn";
 import { Layout } from "./components/Layout";
+import HomePage from "./pages/HomePage";
 
 function App() {
 	return (
 		<AuthContextProvider>
 			<div className="App">
-				<Layout />
+				<Layout>
+					<HomePage />
+				</Layout>
 			</div>
 		</AuthContextProvider>
 	);
