686b909bdaf20ecd6ca156256bf8c18bbf1a7d34
Tejasjagdale <69722269+Tejasjagdale@users.noreply.github.com>
2022-01-22 00:10:13 +0530

    chat working

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { db } from "../utils/firebase-config";
+import { useAuth } from "../context/AuthContext";
 import "./Chat.css";
 
 const Chat = () => {
+  const { currentUser } = useAuth();
   const ref = db.collection("chatdb");
   const [chat, setchat] = useState([]);
   const [data, setData] = useState({
-    sender: "tejas@gmail.com",
+    sender: "",
     receiver: "con1@gmail.com",
     msg: "",
     time: "",
@@ -16,61 +18,79 @@ const Chat = () => {
     ref.onSnapshot((querySnapshot) => {
       const items = [];
       querySnapshot.forEach((doc) => {
-        let temp  = doc.data()
-        temp['id'] = doc.id
-        items.push(temp);
+          if (currentUser) {
+            if (
+                doc.data().sender === currentUser.email ||
+                doc.data().receiver === currentUser.email
+              ) {
+                let temp = doc.data();
+                temp["id"] = doc.id;
+                items.push(temp);
+              }
+          }
       });
       setchat(items);
     });
   }
 
-  function Deletechat(e){
-      e.preventDefault();
-      let id = e.currentTarget.parentElement.id
-      ref.doc(id).delete()
+  function Deletechat(e) {
+    e.preventDefault();
+    let id = e.currentTarget.parentElement.id;
+    ref.doc(id).delete();
   }
 
   const send = async (e) => {
     e.preventDefault();
 
-    var d = new Date(); 
+    var d = new Date();
 
     data.time = d.toLocaleString();
+    data.sender = currentUser.email;
 
-    await ref.add(data)
+    await ref.add(data);
+    setData({
+      sender: "",
+      receiver: "con1@gmail.com",
+      msg: "",
+      time: "",
+    });
   };
 
   useEffect(() => {
     getchats();
-  }, []);
+  }, [currentUser]);
 
   return (
     <>
+        <div className="user_list">
+            
+        </div>
       <div className="mainchat">
-        {console.log(chat)}
         {chat.map((data) => {
-          if (data.sender === "tejas@gmail.com") {
-            return (
-              <div className="chat_wrapper" key={data.id}>
-                <div id={data.id} className="your_msg">
-                  <h2>you</h2>
-                  <p>{data.msg}</p>
-                  <p>{data.time}</p>
-                  <button onClick={Deletechat}>X</button>
+          if (currentUser) {
+            if (data.sender === currentUser.email) {
+              return (
+                <div className="chat_wrapper" key={data.id}>
+                  <div id={data.id} className="your_msg">
+                    <h2>you</h2>
+                    <p>{data.msg}</p>
+                    <p>{data.time}</p>
+                    <button onClick={Deletechat}>X</button>
+                  </div>
                 </div>
-              </div>
-            );
-          } else {
-            return (
-              <div className="chat_wrapper" key={data.id}>
-                <div id={data.id} className="rec_msg">
-                  <h2>{data.sender}</h2>
-                  <p>{data.msg}</p>
-                  <b>{data.time}</b>
-                  <button onClick={Deletechat}>X</button>
+              );
+            } else {
+              return (
+                <div className="chat_wrapper" key={data.id}>
+                  <div id={data.id} className="rec_msg">
+                    <h2>{data.sender}</h2>
+                    <p>{data.msg}</p>
+                    <b>{data.time}</b>
+                    <button onClick={Deletechat}>X</button>
+                  </div>
                 </div>
-              </div>
-            );
+              );
+            }
           }
         })}
       </div>
@@ -78,6 +98,11 @@ const Chat = () => {
         <input
           type="text"
           name="msg"
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              send(e);
+            }
+          }}
           value={data.msg}
           onChange={(e) => {
             setData({ ...data, msg: e.currentTarget.value });
