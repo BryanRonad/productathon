e8fe0d6f0430b3a8316617c81772142ca39a059c
Cris10Lobo <cris10lobo@gmail.com>
2022-01-22 01:10:09 +0530

    working on sos alert

--- src/pages/CouncDashboard.jsx
+++ src/pages/CouncDashboard.jsx
@@ -1,10 +1,28 @@
 import { Grid, GridItem, VStack } from "@chakra-ui/react";
+import { collection, onSnapshot, query, where } from "@firebase/firestore";
 import React from "react";
 import Calander from "../components/Calander";
 import EventCards from "../components/EventCards";
 import SosCard from "../components/SosCard";
+import { db } from "../utils/firebase-config";
 
 const CouncDashboard = () => {
+	const q = query(collection(db, "sessions"), where("cid", "==", ""));
+	const unsubscribe = onSnapshot(q, (snapshot) => {
+		snapshot.docChanges().forEach((change) => {
+			console.log(change.type);
+			if (change.type === "added") {
+				console.log("New city: ", change.doc.data());
+			}
+			if (change.type === "modified") {
+				console.log("Modified city: ", change.doc.data());
+			}
+			if (change.type === "removed") {
+				console.log("Removed city: ", change.doc.data());
+			}
+		});
+	});
+
 	return (
 		<>
 			<Grid
