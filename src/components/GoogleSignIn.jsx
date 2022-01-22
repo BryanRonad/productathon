fdbcbe780012f4c659cc747f553127ecc0700943
Cris10Lobo <cris10lobo@gmail.com>
2022-01-23 00:43:22 +0530

    some ui changes

--- src/components/GoogleSignIn.jsx
+++ src/components/GoogleSignIn.jsx
@@ -15,44 +15,44 @@ import {
 } from "@chakra-ui/react";
 
 function GoogleSignIn() {
-  const navigate = useNavigate();
-  const { id } = useParams();
-  const { signInWithGoogle, currentUser } = useAuth();
-  const [loading, setLoading] = useState(false);
-  const [alreadyreg, setAlreadyReg] = useState(false);
-  const ref1 = db.collection("sessions");
-  const ref = db.collection("users");
-  const [session, setSession] = useState({
-    cid: "",
-    cname: "",
-    time: "",
-    uid: "",
-    uname: "",
-  });
+	const navigate = useNavigate();
+	const { id } = useParams();
+	const { signInWithGoogle, currentUser } = useAuth();
+	const [loading, setLoading] = useState(false);
+	const [alreadyreg, setAlreadyReg] = useState(false);
+	const ref1 = db.collection("sessions");
+	const ref = db.collection("users");
+	const [session, setSession] = useState({
+		cid: "",
+		cname: "",
+		time: "",
+		uid: "",
+		uname: "",
+	});
 
-  const Addsession = () => {
-    var sessioncheck = true;
+	const Addsession = () => {
+		var sessioncheck = true;
 
-    ref1.onSnapshot((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        if (currentUser) {
-          if (doc.data().uid === currentUser.email) {
-            sessioncheck = false;
-          }
-        }
-      });
+		ref1.onSnapshot((querySnapshot) => {
+			querySnapshot.forEach((doc) => {
+				if (currentUser) {
+					if (doc.data().uid === currentUser.email) {
+						sessioncheck = false;
+					}
+				}
+			});
 
-      if (sessioncheck) {
-        var d = new Date();
+			if (sessioncheck) {
+				var d = new Date();
 
-        session.time = d.toLocaleString();
-        session.uid = currentUser.email;
-        session.uname = currentUser.displayName;
+				session.time = d.toLocaleString();
+				session.uid = currentUser.email;
+				session.uname = currentUser.displayName;
 
-        ref1.add(session);
-      }
-    });
-  };
+				ref1.add(session);
+			}
+		});
+	};
 
 	const signInUser = () => {
 		const userCollection = db.collection("users");
@@ -65,33 +65,33 @@ function GoogleSignIn() {
 			.catch((error) => console.log(error));
 	};
 
-  useEffect(() => {
-    if (currentUser) {
-      if (id === "free") {
-        Addsession();
-        setLoading(true);
-      }
-      if (id === "onboard") {
-        navigate("/user/dash");
-      }
-    } else {
-      setLoading(false);
-    }
-  }, [currentUser]);
+	useEffect(() => {
+		if (currentUser) {
+			if (id === "free") {
+				Addsession();
+				setLoading(true);
+			}
+			if (id === "onboard") {
+				navigate("/user/dash");
+			}
+		} else {
+			setLoading(false);
+		}
+	}, [currentUser]);
 
-  useEffect(() => {
-    ref1.onSnapshot((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        if (currentUser) {
-          if (doc.data().uid === currentUser.email) {
-            if (doc.data().cid) {
-              navigate(`/chat?id=${doc.id}`);
-            }
-          }
-        }
-      });
-    });
-  }, [ref1]);
+	useEffect(() => {
+		ref1.onSnapshot((querySnapshot) => {
+			querySnapshot.forEach((doc) => {
+				if (currentUser) {
+					if (doc.data().uid === currentUser.email) {
+						if (doc.data().cid) {
+							navigate(`/chat?id=${doc.id}`);
+						}
+					}
+				}
+			});
+		});
+	}, [ref1]);
 
 	return (
 		<div>
@@ -104,11 +104,12 @@ function GoogleSignIn() {
 					/>
 					<Center h="100px" color="black" fontSize={25}>
 						<CircularProgress
+							size="30px"
 							isIndeterminate
 							color="blue.300"
 							marginRight={10}
 						/>
-						<Text variant="h1">Waiting for a Counsellor...</Text>
+						<Text fontSize="md">Waiting for a counsellor to join....</Text>
 					</Center>
 					<Flex direction="row" justifyContent="space-evenly" mt="30vh">
 						<Button
