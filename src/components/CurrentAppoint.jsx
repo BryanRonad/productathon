fe9243cc05476b6e959bf18de8f3100502825ccf
Cris10Lobo <cris10lobo@gmail.com>
2022-01-23 03:21:17 +0530

    added client chat side

--- src/components/CurrentAppoint.jsx
+++ src/components/CurrentAppoint.jsx
@@ -10,8 +10,28 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import CounCard from "./CounCard";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../utils/firebase-config";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const CurrentAppoint = () => {
+	const { currentUser } = useAuth();
+	const navigate = useNavigate();
+	const getCurrentUrl = async () => {
+		const querySnapshot = await getDocs(collection(db, "paid"));
+		console.log(currentUser.uid);
+		querySnapshot.forEach((doc) => {
+			if (currentUser) {
+				if (doc.data().uid === currentUser.uid.toString()) {
+					navigate(`/chat?id=${doc.id}`);
+				}
+			}
+			// doc.data() is never undefined for query doc snapshots
+			console.log(doc.id, " => ", doc.data());
+		});
+	};
+
 	return (
 		<Container maxW="container.lg">
 			<Flex
@@ -32,33 +52,28 @@ const CurrentAppoint = () => {
 					}}
 				></Box>
 
-				<Box w={2 / 3} p={{ base: 4, md: 4 }}>
+				<Box w={2 / 3} p={{ base: 4, md: 4 }} mt={8}>
 					<Flex alignItems="flex-start" direction="column">
 						<chakra.h1
 							fontSize="2xl"
 							fontWeight="bold"
 							color={useColorModeValue("gray.800", "white")}
 						>
-							Backpack
+							Session active.
 						</chakra.h1>
 						<chakra.p
 							mt={2}
 							fontSize="sm"
 							color={useColorModeValue("gray.600", "gray.400")}
 						>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit In odit
-						</chakra.p>
-						<chakra.p
-							mt={2}
-							fontSize="sm"
-							color={useColorModeValue("gray.600", "gray.400")}
-						>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit In odit
+							Join your ongoing session with a counsellor.
 						</chakra.p>
 					</Flex>
 				</Box>
-				<Box w={1 / 3} p={{ base: 4, md: 4 }} mt={4}>
-					<Button>Join Current Session</Button>
+				<Box w={1 / 3} p={{ base: 4, md: 4 }} mt={12}>
+					<Button onClick={getCurrentUrl} bgColor="blue.200">
+						Join Current Session
+					</Button>
 				</Box>
 			</Flex>
 		</Container>
