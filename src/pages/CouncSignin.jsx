49e5af509f68d7b8a18127daf39e2e0743d4d090
BryanRonad <bryanronad@gmail.com>
2022-01-23 00:11:02 +0530

    Merge true for counsellors

--- src/pages/CouncSignin.jsx
+++ src/pages/CouncSignin.jsx
@@ -6,29 +6,32 @@ import { useAuth } from "../context/AuthContext";
 import SignInCard from "../components/SignInCard";
 
 const CouncSignin = () => {
-	const { signInWithGoogle } = useAuth();
-	const navigate = useNavigate();
+  const { signInWithGoogle } = useAuth();
+  const navigate = useNavigate();
 
-	const signInCounsellor = () => {
-		const counsellorCollection = db.collection("counsellors");
-		signInWithGoogle()
-			.then((user) => {
-				console.log(user.user.uid);
-				counsellorCollection.doc(user.user.uid.toString()).set({
-					isVerified: false,
-					bio: "This is the bio",
-					qualifications: ["MA", "PhD"],
-				});
-				navigate("/counsellor/verify");
-			})
-			.catch((error) => console.log(error));
-	};
+  const signInCounsellor = () => {
+    const counsellorCollection = db.collection("counsellors");
+    signInWithGoogle()
+      .then((user) => {
+        console.log(user.user.uid);
+        counsellorCollection.doc(user.user.uid.toString()).set(
+          {
+            isVerified: false,
+            bio: "This is the bio",
+            qualifications: ["MA", "PhD"],
+          },
+          { merge: true }
+        );
+        navigate("/counsellor/verify");
+      })
+      .catch((error) => console.log(error));
+  };
 
-	return (
-		<>
-			<SignInCard signInCounsellor={signInCounsellor} />
-		</>
-	);
+  return (
+    <>
+      <SignInCard signInCounsellor={signInCounsellor} />
+    </>
+  );
 };
 
 export default CouncSignin;
